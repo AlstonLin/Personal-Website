9132eb26ae452d359ee4af7989a490fa007d9eb8
Alston Lin <alstonlin812@gmail.com>
2017-05-24 20:26:02 -0400

    Added email forwarding to AWS SES

--- client/static/js/sendemail.js
+++ client/static/js/sendemail.js
@@ -1,43 +1,37 @@
-function validateForm()
-  {
-   "use strict";  
-      var title = $("#name").val();
-      var err=true;
-      if (title=="" || title==null) {  
-        $("#name").addClass("validation");
-      
-        var err= false;
-      } else {  $("#name").removeClass("validation");}
-      var email = $("#email").val();
-      if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
-       $("#email").addClass("validation"); 
-       
-         var err= false;
-      } else {  $("#email").removeClass("validation"); }
-      var title = $("#message").val();
-      if (title=="" || title==null) { 
-        $("#message").addClass("validation"); 
-         var err= false;
-      } else{ $("#message").removeClass("validation"); }
-    return err;
+function validateForm(){
+  $("#name").removeClass("validation");
+  $("#email").removeClass("validation");
+  $("#message").removeClass("validation");
+  var valid = true;
+  if (!$("#name").val()) {  
+    $("#name").addClass("validation");
+    valid = false;
   }
-  $(document).ready(function(){
-    "use strict"; 
-    $("#button").click(function(e){
-      if(validateForm()){
-       e.preventDefault();
-        $.ajax({type: "POST",
-                url: "send-email.php",
-                data:$("#ContactForm").serialize(),
-                success:function(result){
-                  $("#successmsg").html(result);
-                  $("#name").val('');
-                  $("#email").val('');
-                  $("#message").val('');
-              }}); 
-     } else{
-      return false;
-     }
-    });
+  if (!(/(.+)@(.+){2,}\.(.+){2,}/.test($("#email").val()))) {
+    $("#email").addClass("validation"); 
+    valid = false;
+  } 
+  if (!$("#message").val()) { 
+    $("#message").addClass("validation"); 
+    valid = false;
+  }
+  return valid;
+}
+$(document).ready(function(){
+  $("#sendEmail").click(function(e){
+    e.preventDefault();
+    if (validateForm()){
+      $.ajax({
+        type: "POST",
+        url: "/sendEmail",
+        data: $("#contactForm").serialize(),
+        success:function(result){
+          $("#successmsg").html(`<p class='success'>${result}<\p>`);
+          $("#name").val('');
+          $("#email").val('');
+          $("#message").val('');
+        }
+      }); 
+    }
   });
-      
+});
--- server/index.js
+++ server/index.js
@@ -1,9 +1,44 @@
 const express = require('express'),
+      bodyParser = require('body-parser'),
       app = express(),
       PORT = process.env.PORT || 3000,
-      path = require('path');
+      EMAIL_ADDRESS_FROM = "contact@alstonlin.io",
+      EMAIL_ADDRESS_TO = "alstonlin812@gmail.com",
+      path = require('path'),
+      aws = require('aws-sdk'),
+      ses = new aws.SES({ region: "us-east-1" });
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'client', 'static')));
+
+app.post('/sendEmail', function(req, res){
+  ses.sendEmail({
+    Source: `${req.body.name} <${EMAIL_ADDRESS_FROM}>`,
+    Destination: {
+      ToAddresses: [EMAIL_ADDRESS_TO]
+    },
+    Message: {
+      Subject: {
+        Charset: "UTF-8",
+        Data: `alstonlin.io Contact`
+      },
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: req.body.message
+        }
+      },
+    },
+    ReplyToAddresses: [req.body.email],
+  }, function(err, data){
+    if (err){
+      console.error(err);
+      return res.status(500).send("An error has occurred when sending the message");
+    }
+    res.status(200).send("Your message has been sent");
+  });
+});
+
 app.listen(PORT, function(){
   console.log(`Serving website at port ${PORT}`);
 });
